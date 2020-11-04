a26d303c9d1603226648b45acc8f677b15dfc519
juzQrios <dj30c.1@gmail.com>
2020-11-04 19:33:11 +0530

    Add DisplayWeather component

---
+++ src/Components/DisplayWeather/DisplayWeather.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class DisplayWeather extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+    <section className="DisplayWeather">
+      Weather Data Display
+    </section>
+    )
+  }
+}
+
+export default DisplayWeather;
