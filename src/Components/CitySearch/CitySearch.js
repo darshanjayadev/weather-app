7c00df078557be4b3033de196312b7395fcc79d1
juzQrios <dj30c.1@gmail.com>
2020-11-04 19:32:37 +0530

    Add CitySearch component

---
+++ src/Components/CitySearch/CitySearch.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import cityList from 'indian-cities-json';
+
+class CitySearch extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      cityInput: "",
+      isSelectVisible: false,
+    }
+    this.handleCityInputChange = this.handleCityInputChange.bind(this);
+    this.handleCitySelect = this.handleCitySelect.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleCityInputChange(e) {
+    this.setState({
+      cityInput: e.target.value,
+      isSelectVisible: true
+    });
+  }
+
+  handleCitySelect(e) {
+    this.setState({
+      isSelectVisible: false,
+      cityInput: e.target.value
+    })
+    this.props.onSelect(e.target.value)
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+  }
+
+  render() {
+    const filteredList = cityList.cities.filter((city) => {
+      return city.name.toLowerCase().search(this.state.cityInput.toLowerCase()) !== -1;
+    });
+    return (
+      <article className="CitySearch">
+        <form onSubmit={this.handleSubmit}>
+          <input type="text" value={this.state.cityInput} onChange={this.handleCityInputChange} placeholder="Enter a city name" />
+          {
+            this.state.isSelectVisible && !this.state.cityInput == "" &&
+            <select value={this.state.selectedCity} onChange={this.handleCitySelect} multiple>
+              {
+                filteredList.slice(0,5).map((city) => (
+                  <option key={city.name} value={`${city.name}, ${city.state}`}>{city.name},{city.state}</option>
+                ))
+              }
+            </select>
+          }
+        </form>
+      </article>
+    )
+  }
+}
+
+export default CitySearch;
